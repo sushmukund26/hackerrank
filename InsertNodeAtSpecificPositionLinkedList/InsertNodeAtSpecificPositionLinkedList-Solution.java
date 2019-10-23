bb1cde2b29e2934a1629a7eceb9b0be1dec52cba
Sush Mukund <msushmethaa@vt.edu>
2019-10-22 22:04:03 -0400

    Insert Node at specific position of Linked list - Problem Statement and Solution

---
+++ InsertNodeAtSpecificPositionLinkedList/InsertNodeAtSpecificPositionLinkedList-Solution.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        int index = 0;
+        SinglyLinkedListNode curr = head;
+        while(index < position-1 && curr.next != null) {
+            curr = curr.next;
+            index++;
+        }
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+        newNode.next = curr.next;
+        curr.next = newNode;
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
