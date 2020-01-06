076ab2554a61b615fd5574d7c5628795f92e9b5b
Sush Mukund <msushmethaa@vt.edu>
2020-01-06 07:20:38 -0500

    Level order tree traversal - Solution

---
+++ LevelOrderTreeTraversal/LevelOrderTreeTraversal-Solution.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	public static void levelOrder(Node root) {
+        Queue<Node> nodes = new LinkedList<Node>();
+        nodes.add(root);
+        while(!nodes.isEmpty()) {
+            Node curr = nodes.poll();
+            System.out.print(curr.data + " ");
+            if(curr.left != null) {
+                nodes.add(curr.left);
+            }
+            if(curr.right != null) {
+                nodes.add(curr.right);
+            }
+        }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}
