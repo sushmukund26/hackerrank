414fb1f16fad1b8b6c355a1d4cd9901f306512b6
Sush Mukund <msushmethaa@vt.edu>
2020-01-08 13:47:07 -0500

    Merge two sorted linked lists - Solution

---
+++ MergeTwoSortedLinkedLists/MergeTwoSortedLinkedLists-Solution.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        //Create head with the smallest element
+        SinglyLinkedListNode head;
+        if(head1.data < head2.data) {
+            head = new SinglyLinkedListNode(head1.data);
+            head1 = head1.next;
+        } else {
+            head = new SinglyLinkedListNode(head2.data);
+            head2 = head2.next;
+        }
+        SinglyLinkedListNode ptr = head;
+
+        //Iterate both lists, compare and build the new sorted list
+        while(head1 != null && head2 != null) {
+            if(head1.data < head2.data) {
+                ptr.next = new SinglyLinkedListNode(head1.data);
+                ptr = ptr.next;
+                head1 = head1.next;
+            } else {
+                ptr.next = new SinglyLinkedListNode(head2.data);
+                ptr = ptr.next;
+                head2 = head2.next;
+            }
+        }
+
+        //In cases where the lists are of unequal lengths, add the remaining elements of the longer list to the new list
+        while(head1 != null) {
+            ptr.next = new SinglyLinkedListNode(head1.data);
+            ptr = ptr.next;
+            head1 = head1.next;
+        }
+
+        while(head2 != null) {
+            ptr.next = new SinglyLinkedListNode(head2.data);
+            ptr = ptr.next;
+            head2 = head2.next;
+        }
+
+        return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist1Count; i++) {
+                int llist1Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist1.insertNode(llist1Item);
+            }
+          
+          	SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist2Count; i++) {
+                int llist2Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist2.insertNode(llist2Item);
+            }
+
+            SinglyLinkedListNode llist3 = mergeLists(llist1.head, llist2.head);
+
+            printSinglyLinkedList(llist3, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
