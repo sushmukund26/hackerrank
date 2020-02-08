5e24c21190da2ad72df30a7b1dbda79eaa2e45b4
Sush Mukund <msushmethaa@vt.edu>
2020-02-08 16:19:50 -0500

    Java primality test - Solution

---
+++ JavaPrimalityTest/JavaPrimalityTest-Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        BigInteger num = new BigInteger(n);
+        System.out.println(num.isProbablePrime(1) ? "prime" : "not prime");
+        scanner.close();
+    }
+}
